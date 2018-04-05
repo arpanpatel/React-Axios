5a318a8e5af0fdd598c6375f3a1604e50b9eda4e
arpan patel <patelarpan3832@gmail.com>
2018-04-05 22:32:57 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Axios
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { render } from 'react-dom';
+import React, { Component } from 'react';
+import axios from 'axios'
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://randomuser.me/api/?results=50')
+      .then(response => {
+        const data = response.data.results;
+        this.setState({ data })
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        {this.state.data.map((item, index) => <UserList key={index} {...item} />)}
+      </div>
+    );
+  }
+}
+
+const UserList = (props) => (
+  <p><strong>name : </strong> {props.name.first}</p>
+)
+
+render(<App />, document.getElementById('root'));
